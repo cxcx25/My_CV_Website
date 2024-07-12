5f84d429173c179b1034b553452140f468eb1540
cxcx25 <90252777+cxcx25@users.noreply.github.com>
2024-07-12 12:13:13 +0000

    1st rev

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 // script.js
 document.addEventListener('DOMContentLoaded', function() {
     const links = document.querySelectorAll('nav ul li a');
+    const contentContainer = document.getElementById('content-container');
 
-    for (const link of links) {
-        link.addEventListener('click', smoothScroll);
-    }
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault(); 
+            const target = this.dataset.target; 
 
-    function smoothScroll(event) {
-        event.preventDefault();
-        const targetId = event.currentTarget.getAttribute('href').substring(1);
-        const targetSection = document.getElementById(targetId);
-
-        window.scrollTo({
-            top: targetSection.offsetTop,
-            behavior: 'smooth'
+            getContent(target); 
         });
+    });
+
+    function getContent(target) {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', `image/${target}.html`, true);
+
+        xhr.onload = function() {
+            if (xhr.status >= 200 && xhr.status < 400) {
+                contentContainer.innerHTML = xhr.responseText; 
+            } else {
+                console.error("Request failed. Status:", xhr.status);
+            }
+        };
+
+        xhr.onerror = function() {
+            console.error("Error occurred during request.");
+        };
+
+        xhr.send();
     }
 });
